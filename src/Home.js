52d06367dc9f52490a976d306359bed8b6681528
aarminoe <aarminoe@gmail.com>
2023-02-27 16:07:30 -0600

    css added to home page

--- src/Home.js
+++ src/Home.js
@@ -1,15 +1,16 @@
 import React from "react";
+import { Card } from "@mui/material";
 
 function Home() {
 
     return(
-        <div className="home-page">
+        <Card className="home-page">
             <h1 className="welcome-title-home">Welcome!</h1>
             
-            <div>
+            <div className="splash-message">
                 Safe Space is a a completely anonymous blog board. Feel free to share anything you would like! You may find other bloggers you can relate to. Please be kind and enjoy yourself, this is a safe space!
             </div>
-        </div>
+        </Card>
     )
 }
 
